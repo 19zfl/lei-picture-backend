d1b681bec0c3bed009d0fd8c1d66a7cd7a57aa07
19zfl <19aleiya@gmail.com>
2025-04-28 20:31:44 +0800

    用户 Mapper

---
+++ src/main/java/com/zfl19/leipicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.zfl19.leipicturebackend.mapper;
+
+import com.zfl19.leipicturebackend.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zengf
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-04-28 10:01:18
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
